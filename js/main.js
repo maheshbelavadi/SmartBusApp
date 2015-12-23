98118473c5e7c969db3d8d88bec863b85ae1dae2
maheshbelavadi <mahesh.belavadi@gmail.com>
2015-12-24 01:02:43 +0530

    Alert hardcoded

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = {
 
     showAlert: function (message, title) {
+	alert(title ? (title + ": " + message) : message);
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
